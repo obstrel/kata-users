87a8fdca3557034e2eafe3680dc6cb26ed85ff37
serge <sergey.xi@yandex.ru>
2025-09-13 20:51:58 +0300

    refactor: moved toString to the and of file

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -7,16 +7,6 @@ import javax.persistence.Table;
 @Table
 public class User {
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
     @Id
     private Long id;
 
@@ -69,5 +59,15 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
